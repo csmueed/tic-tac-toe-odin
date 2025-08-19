fa7ccfa7b8cc710d1d6b49ac02aab55efa9c92ea
csmueed <csmueed@gmail.com>
2025-08-19 11:49:05 +0500

    basic layout structure

---
+++ script.js
@@ -0,0 +1,16 @@
+const r11 = document.querySelector(".r11");
+const r12 = document.querySelector(".r12");
+const r13 = document.querySelector(".r13");
+
+const r21 = document.querySelector(".r21");
+const r22 = document.querySelector(".r22");
+const r23 = document.querySelector(".r23");
+
+const r31 = document.querySelector(".r31");
+const r32 = document.querySelector(".r32");
+const r33 = document.querySelector(".r33");
+
+const rrr = document.querySelectorAll(".rrr")
+
+
+
